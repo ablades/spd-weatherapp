1e161c77d10aec83977547035ec0addf456cd2df
ablades <audaris@gmail.com>
2020-06-02 14:50:30 -0700

    update args parsing

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 	"log"
 	"net/http"
 	u "net/url"
+	"os"
 )
 
 func apiRequest(cityName string, apiKey string) {
 
 	// build url query string
 	url, _ := u.Parse("api.openweathermap.org/data/2.5/weather")
+	url.Scheme = "https"
 	q := url.Query()
-	q.Add("q", cityName)
 	q.Add("appid", apiKey)
+	q.Add("q", cityName)
 	url.RawQuery = q.Encode()
 
 	// Request
+	fmt.Println(url.String())
 	r, err := http.Get(url.String())
 	if err != nil {
 		log.Fatal(err)
@@ -28,9 +31,11 @@ func apiRequest(cityName string, apiKey string) {
 
 	content, _ := ioutil.ReadAll(r.Body)
 
-	fmt.Println(content)
+	fmt.Println(string(content))
 }
 
 func main() {
+	args := os.Args
+	apiRequest(args[1], args[2])
 
 }
