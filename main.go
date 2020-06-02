856982434947f754336672d3c9d83785e85206b5
ablades <audaris@gmail.com>
2020-06-02 15:26:02 -0700

    add weather stats struct

--- main.go
+++ main.go
@@ -8,9 +8,41 @@ import (
 	"net/http"
 	u "net/url"
 	"os"
+	"time"
 )
 
-func apiRequest(cityName string, apiKey string) {
+// url to pull images http://openweathermap.org/img/wn/10d@2x.png
+
+// struct that holds relevant weather data
+type weatherStats struct {
+	Weather []struct {
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+	Name string `json:"name"`
+}
+
+// Get current time a user submits an entry
+func getTime() {
+	fmt.Println(time.Now())
+}
+
+//Send request to weather api
+func apiRequest(cityName string, apiKey string) []byte {
 
 	// build url query string
 	url, _ := u.Parse("api.openweathermap.org/data/2.5/weather")
@@ -31,11 +63,11 @@ func apiRequest(cityName string, apiKey string) {
 
 	content, _ := ioutil.ReadAll(r.Body)
 
-	fmt.Println(string(content))
+	return content
 }
 
 func main() {
 	args := os.Args
 	apiRequest(args[1], args[2])
-
+	getTime()
 }
